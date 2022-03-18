c2e0276add78fb597e17d405b4e490368128effa
Charu1611 <bansalc981@gmail.com>
2022-03-18 13:20:10 +0530

    update

--- complaintportal/settings.py
+++ complaintportal/settings.py
@@ -127,7 +127,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
 
 
 
