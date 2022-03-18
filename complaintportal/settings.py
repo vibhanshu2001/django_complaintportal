fa1d27194476c5c40ea06d75c72d29584dadf438
Charu1611 <bansalc981@gmail.com>
2022-03-18 13:18:40 +0530

    update

--- complaintportal/settings.py
+++ complaintportal/settings.py
@@ -125,7 +125,7 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
 
