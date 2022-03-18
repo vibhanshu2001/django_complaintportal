00b33655953ee2aab4bfdddd1418fa6328e3b6a7
Charu1611 <bansalc981@gmail.com>
2022-03-18 13:37:51 +0530

    update

--- complaintportal/urls.py
+++ complaintportal/urls.py
@@ -5,3 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
 ]
+admin.site.site_header = "Blue Bird Public School"
+admin.site.site_title = "Major Admin Panel"
+admin.site.index_title = "Welcome Admin to BBPS Portal"
