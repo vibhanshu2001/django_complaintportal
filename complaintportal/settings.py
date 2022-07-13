d527fb69236dc6ea585b6558d1b380641b8ef984
Charu1611 <bansalc981@gmail.com>
2022-07-13 18:18:52 +0530

    update

--- complaintportal/settings.py
+++ complaintportal/settings.py
@@ -123,23 +123,23 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_URL = '/static/'
-
-
-
-
-
-# MEDIA_ROOT =  os.path.join(BASE_DIR, 'media/')
-# MEDIA_URL = '/media/'
-
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+
+
+
+
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
+
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# MEDIA_ROOT =  os.path.join(BASE_DIR, 'media/')
+# MEDIA_URL = '/media/'
